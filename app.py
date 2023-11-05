ee8bcf276553ecad44ffadfff8ec2531a15d3490
davronovtatu <davronovtatu@gmail.com>
2023-11-05 21:30:51 +0500

    wikipedia bot

---
+++ app.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+import wikipedia
+wikipedia.set_lang('uz')
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "5173776534:AAGq6UIAviGFSVZ6kcomdymLgQd4l5x5efc"
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    try:
+        txt=message.text
+        data=wikipedia.summary(txt)
+        await message.reply(data)
+    except Exception as e:
+        await message.answer("Bunday ma'lumot topilmadi")
+
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
